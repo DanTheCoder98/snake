66eabf1ae44b4c8266a66661d2264d873a7a60df
Daniel Sarney <129965299+CoderDan98@users.noreply.github.com>
2024-10-25 13:59:09 +0100

    Snake Segments

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+snake_segments = []
 
+start_x = 0
+
+for turtles in range(3):
+    new_turtle = turtle.Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.penup()
+    new_turtle.goto(start_x, 0)
+
+    start_x -= 20
+    snake_segments.append(new_turtle)
 
 screen.exitonclick()
