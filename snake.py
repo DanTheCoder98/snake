6e7283c3e0fd27825f9944790c4c73e79a922d35
Daniel Sarney <daniel_sarney@live.co.uk>
2024-10-29 17:44:18 +0000

    Snake reset

--- snake.py
+++ snake.py
@@ -7,6 +7,7 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
     def __init__(self):
         self.segments = []
@@ -32,7 +33,7 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        
+
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
@@ -51,3 +52,9 @@ class Snake:
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
